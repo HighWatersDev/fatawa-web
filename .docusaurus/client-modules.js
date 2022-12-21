f734f0a4a872ebee434b0c8a88f10aa574cced54
Rinat Kirimov <37680855+HighWatersDev@users.noreply.github.com>
2022-12-20 22:51:46 -0500

    init commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/luqmaan/projects/fatawa-web/node_modules/infima/dist/css/default/default-rtl.css'),
+  require('/home/luqmaan/projects/fatawa-web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/luqmaan/projects/fatawa-web/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/luqmaan/projects/fatawa-web/src/css/custom.css'),
+];
