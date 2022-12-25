3c5ba2d79bdda24c952e5fbe9fe9e6fbec469da6
Rinat Kirimov <37680855+HighWatersDev@users.noreply.github.com>
2022-12-25 09:41:07 -0500

    update web

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -7,7 +7,7 @@ export default {
   "title": "فتاوى علماء أهل السنة",
   "tagline": "",
   "url": "https://salafifatawa.azurewebsites.net",
-  "baseUrl": "/en/",
+  "baseUrl": "/",
   "onBrokenLinks": "throw",
   "onBrokenMarkdownLinks": "warn",
   "favicon": "img/fav2.ico",
@@ -65,7 +65,7 @@ export default {
   ],
   "themeConfig": {
     "navbar": {
-      "title": "Homepage",
+      "title": "الرئيسية",
       "logo": {
         "alt": "My Site Logo",
         "src": "img/logo2.jpeg"
@@ -75,7 +75,7 @@ export default {
           "type": "doc",
           "docId": "intro",
           "position": "left",
-          "label": "Fatawa"
+          "label": "الفتاوى"
         },
         {
           "type": "localeDropdown",
